a19c67c8e4bf7fe0d0ad1716e0699c784bcbeb62
DR-PC61059 <pramhathai2537@gmail.com>
2017-11-06 16:15:24 +0700

    Create Fragment OK

---
+++ app/src/main/java/app/suk/pramhathai/rmutsvservice/fragment/MainFragment.java
@@ -0,0 +1,28 @@
+package app.suk.pramhathai.rmutsvservice.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import app.suk.pramhathai.rmutsvservice.R;
+
+/**
+ * Created by DR-PC61059 on 6/11/2560.
+ */
+
+public class MainFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_main,container,false);
+
+        return view;
+    }
+}   // Main Class
