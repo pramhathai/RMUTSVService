0ce2f112519431c4432a680d2ddb17181947bb3e
DR-PC61059 <pramhathai2537@gmail.com>
2017-11-09 15:32:43 +0700

    delete 9nov

---
+++ app/src/main/java/app/suk/pramhathai/rmutsvservice/utility/DeleteData.java
@@ -0,0 +1,47 @@
+package app.suk.pramhathai.rmutsvservice.utility;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+/**
+ * Created by DR-PC61059 on 9/11/2560.
+ */
+
+public class DeleteData extends AsyncTask<String, Void, String>{
+
+    private Context context;
+
+    public DeleteData(Context context){
+        this.context = context;
+    }
+
+
+    @Override
+    protected String doInBackground(String... strings) {
+
+        try {
+
+            OkHttpClient okHttpClient = new OkHttpClient();
+            RequestBody requestBody = new FormEncodingBuilder()
+                    .add("isAdd", "true")
+                    .add("Name", strings[0])
+                    .build();
+            Request.Builder builder = new Request.Builder();
+            Request request = builder.url(strings[1]).post(requestBody).build();
+            Response response = okHttpClient.newCall(request).execute();
+            return response.body().string();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        return null;
+    }
+}   //Main Class
--- app/src/main/java/app/suk/pramhathai/rmutsvservice/utility/MyConstant.java
+++ app/src/main/java/app/suk/pramhathai/rmutsvservice/utility/MyConstant.java
@@ -10,6 +10,12 @@ public class MyConstant {
 
     private String urlGatAllUser = "http://androidthai.in.th/rmuts/getAllDataMaster.php";
 
+    private String urldeleteData = "http://androidthai.in.th/rmuts/deleteDataMaster.php";
+
+    public String getUrldeleteData(){
+        return  urldeleteData;
+    }
+
     public String getUrlGatAllUser() {
         return urlGatAllUser;
     }
